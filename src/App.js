8bdc9f694e52557d4efbdcf26a50cdab0e405a7e
Ev1lSoda <killer.be.29@gmail.com>
2022-11-11 11:57:41 +0200

    initialized data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import GridComponent from './GridComponent/GridComponent';
+import data from './data.json'
+import columns from './columns.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>DHX-TEST-TASK</h1>
       </header>
+      <main>
+        <GridComponent data={data} columns={columns} />
+      </main>
     </div>
   );
 }
---
+++ src/GridComponent/GridComponent.js
@@ -0,0 +1,13 @@
+import './GridComponent.css';
+
+function GridComponent(props) {
+  const {columns, data} = props;  
+
+  return (
+    <div className="main__grid">
+        <h1>DHX-TEST-TASK</h1>
+    </div>
+  );
+}
+
+export default GridComponent;
